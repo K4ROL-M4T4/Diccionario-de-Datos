5487734e05c02c72155a8b8a0c0872aa8f03b465
marianvegto <marianvegto@gmail.com>
2026-04-23 22:30:16 -0600

    Implement menu for entity management

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Vega Torres Marian
+//Tovar Mata Karol
+void menuEntidades(CDiccionario &dic) {
+    int op;
+    do {
+        cout << "\n--- MENU ENTIDADES ---" << endl;
+        cout << "1. Nueva Entidad\n2. Consultar Entidades\n3. Eliminar Entidad\n4. Modificar Entidad\n5. Menu Atributos\n6. Regresar" << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> op;
+
+        switch(op) {
+            case 1: dic.altaEntidad(); break;
+            case 2: dic.consultaEntidades(); break;
+            case 3: dic.bajaEntidad(); break;
+            case 4: dic.modificaEntidad(); break;
+            case 5: menuAtributos(); break;
+        }
+    } while (op != 6);
+}
